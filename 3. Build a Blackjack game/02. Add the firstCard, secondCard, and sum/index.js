ef4415efaa5dcfd9c763d05b125e517933b21ba2
raigal-r <rqlcrrsc@gmail.com>
2023-06-03 14:24:25 +0300

    Back to study

--- 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
@@ -1,4 +1,61 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive
+let message = ""
+
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random() *13) + 1
+    if (randomNumber === 1) {
+        return 11
+    } else if (randomNumber > 10){
+        return 10
+    } else {
+        return(randomNumber)
+    }
+}
+
+function startGame(){
+    let isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+   renderGame()
+}
+
+function renderGame(){
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent= "Cards: "
+    for (let i = 0; i < cards.length; i++ ) {
+        cardsEl.textContent += cards[i] + " "
+
+    }
+    if (sum <= 20) {
+        message = ("Do you want to draw a new card?")
+    } else if (sum === 21) {
+        message =("Wohoo! You've got Blockjack!")
+        hasBlackJack = true
+    } else {
+        message =("You`re out of the game!")
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    if (isAlive===true && hasBlackJack===false){
+        console.log("Drawing a new card from the deck!")
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+
+}
